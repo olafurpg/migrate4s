23c9aa61e777473329d06dc52a735ae0a39d2d96
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-06-02 13:26:03 +0200

    Play around with extends App

--- scalafix-core/src/main/scala/scalafix/rewrite/NoExtendsApp.scala
+++ scalafix-core/src/main/scala/scalafix/rewrite/NoExtendsApp.scala
@@ -1,35 +1,47 @@
 package scalafix
 package rewrite
 
-import scala.meta.{ Symbol => _, _ }
+import scala.meta.{Symbol => _, _}
 import scalafix.syntax._
-import scalafix.util.{ Whitespace => _, _ }
+import scalafix.util.{Whitespace => _, _}
 import scala.meta.contrib._
 import scala.meta.tokens.Token._
 import scala.meta.semantic.Signature
 import scala.collection.mutable
+import org.scalameta.logger
 
 case class NoExtendsApp(mirror: Mirror) extends SemanticRewrite(mirror) {
 
   override def rewrite(ctx: RewriteCtx): Patch = {
+    def getTemplateCurlyTokens(template: Template): Tokens = {
+      val tokens = template.tokens
+      val result = for {
+        close <- tokens.lastOption
+        if close.is[RightBrace]
+        open <- ctx.matching.open(close.asInstanceOf[RightBrace])
+      } yield tokens.dropWhile(_.pos.start.offset < open.pos.start.offset)
+      result.get
+    }
+    def indent(tokens: Tokens, n: Int): Patch =
+      tokens.collect {
+        case nl @ Newline() => ctx.addRight(nl, " " * n)
+      }.asPatch
     ctx.tree.collect {
-      case t: Defn.Object
-        if t.templ.parents.exists { case c: Ctor.Ref => c.symbolOpt.map(_.normalized.syntax) == Some("_root_.scala.App.") } && t.templ.stats.isDefined =>
-          println(t.templ.tokens.structure)
-          val (head, stats) = t.templ.tokens.span(_.is[LeftBrace])
-          println(head)
-          println(stats)
-          val indentation = t.templ.stats.get.head.tokens.takeWhile(_.is[Whitespace])
-          println(s">>>>>${indentation}<<<<")
-          val open = ctx.addLeft(t.templ.stats.get.head.tokens.head, "def main(args: Array[String]) = {\n  ")
-          val close = ctx.addRight(t.templ.stats.get.last.tokens.last, "\n}")
-          val indent = t.templ.stats.get.foldLeft(Patch.empty)((patch, s) => patch + s.tokens.collect { case tok @ Newline() => ctx.addRight(tok, "  ") }.asPatch)
-          open + close + indent
+      case t: Defn.Object if t.templ.parents.exists {
+            case c: Ctor.Ref =>
+              c.symbolOpt
+                .map(_.normalized.syntax)
+                .contains("_root_.scala.App.")
+          } && t.templ.stats.isDefined =>
+        val tokens = t.templ.tokens
+        val firstStatToken = t.templ.stats.get.head.tokens.head
+        indent(tokens, 2) +
+          ctx.addRight(tokens.last, "\n}") +
+          ctx.addLeft(firstStatToken,
+                      "def main(args: Seq[String]): Unit = {\n    ")
     }.asPatch
   }
 }
-
-
 // object Foo {
 //   println()
 // }
@@ -39,4 +51,3 @@ case class NoExtendsApp(mirror: Mirror) extends SemanticRewrite(mirror) {
 //     println()
 //   }
 // }
-
